43e3f002586739f7f9c0c25aa1c7c3cc27fe953f
stantoncbradley <stantoncbradley@gmail.com>
2017-10-02 15:44:03 -0700

    use default import in require

--- moment-timezone.js
+++ moment-timezone.js
@@ -11,7 +11,7 @@
 	if (typeof define === 'function' && define.amd) {
 		define(['moment'], factory);                 // AMD
 	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory(require('moment')); // Node
+		module.exports = factory(require('moment').default); // Node
 	} else {
 		factory(root.moment);                        // Browser
 	}
